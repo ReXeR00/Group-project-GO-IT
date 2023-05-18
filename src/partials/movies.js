c4efd98e550c668ea3416425d7e625baee636ba1
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-05-18 21:02:22 +0200

    movie card & template

---
+++ src/partials/movies.js
@@ -0,0 +1,17 @@
+const moviesListEl = document.querySelector('.movies__list');
+
+const movieElement = `
+  <li class="movies__element">
+    <figure>
+      <img src="https://pixabay.com/get/g396ade0bfc4dd9a2b944e85069ca97d74b7dfbef6f1951581c6af25d6fad9a4cb9aef85767f8242b1eb497e80d5c339688ec58a9efe877b8df700048b1c2a5ce_1280.jpg" alt="opis filmu" class="movies__poster">
+      <figcaption>
+        <p class="movies__title">Greyhound</p>
+        <p class="movies__type">Drama, Action | <span class="movies__year">2020</span></p>
+       </figcaption>
+    </figure>
+  </li>
+    `;
+
+for (let i = 0; i < 20; i++) {
+  moviesListEl.insertAdjacentHTML('afterbegin', movieElement);
+}
