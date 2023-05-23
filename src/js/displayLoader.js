f57f3977f395bf62e59eddf0e97e4b955b58478a
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-05-23 20:11:02 +0200

    change in js

--- src/js/displayLoader.js
+++ src/js/displayLoader.js
@@ -1,10 +1,10 @@
 const loader = document.querySelector('.loader');
 
 function displayLoader(element) {
-  if (element.style.display === 'none') {
-    element.style.display = 'block';
-  } else {
+  if (element.style.display === 'block') {
     element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
   }
 }
 
