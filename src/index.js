c2262aa0193121ee70ea0bacdbdf87846e05e778
ReXeR00 <106077454+ReXeR00@users.noreply.github.com>
2023-05-26 15:06:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import './js/movies';
 import './js/modal-team';
 import './js/variables';
 import './js/trending';
-
-import './js/trending';
-
-
 import './js/modalMovie';
 import './js/input-search';
-
 import './js/pagination';
-
