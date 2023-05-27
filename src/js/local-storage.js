f70278d14d8c9a20ab962cde095537ca4a45584a
Szymon Oponczewski <szymon_o@mac.com>
2023-05-27 10:49:59 +0200

    localStorage_test_szymon

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,20 +1,45 @@
-export const setToLocalStorage = (key, value) => {
-  try {
-    localStorage.setItem(key, JSON.stringify(value));
-  } catch (error) {
-    console.log(error.messege);
-  }
+const localStorageKeys = {
+  WATCHED: 'watchedMovies',
+  QUEUE: 'movieQueue',
 };
-export const getFromStorage = key => {
+
+const getFromStorage = key => {
   try {
-    const serialisedState = localStorage.getItem(key);
-    return serialisedState === null ? undefined : JSON.parse(serialisedState);
+    const serializedValue = localStorage.getItem(key);
+    return JSON.parse(serializedValue);
   } catch (error) {
-    console.log(error.messege);
+    console.log(error.message);
+    return null;
   }
 };
 
-export const localStorageKeys = {
-  WATCHED: 'watched',
-  QUEUE: 'queue',
+export const setToLocalStorage = (key, value) => {
+  try {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const addToWatched = film => {
+  try {
+    let watchedFilms = getFromStorage(localStorageKeys.WATCHED) || [];
+    watchedFilms.push(film);
+    setToLocalStorage(localStorageKeys.WATCHED, watchedFilms);
+    console.log('Film added to Watched:', film);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const addToQueue = film => {
+  try {
+    let filmQueue = getFromStorage(localStorageKeys.QUEUE) || [];
+    filmQueue.push(film);
+    setToLocalStorage(localStorageKeys.QUEUE, filmQueue);
+    console.log('Film added to Queue:', film);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
