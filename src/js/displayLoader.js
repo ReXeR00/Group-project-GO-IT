67ef38610e95820757bde590557e679c84f0a083
Emil Nowak <emil32132@onet.pl>
2023-05-29 22:02:31 +0200

    del console.log in displayLoader

--- src/js/displayLoader.js
+++ src/js/displayLoader.js
@@ -3,10 +3,8 @@ const loader = document.querySelector('.loader');
 function displayLoader(element) {
   if (element.style.display === 'block') {
     element.style.display = 'none';
-    console.log('loader-stop');
   } else {
     element.style.display = 'block';
-    console.log('loader-start');
   }
 }
 
